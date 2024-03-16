3fa94649b01d927f163b79541997b38eeefea23a
Daniele <47061226+Daniele06@users.noreply.github.com>
2024-03-16 15:00:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// Section links
+const sectionsLinks = [];
+// Specify number of links
+for (let i = 1; i <= 5; i++) {
+  sectionsLinks.push(document.getElementById(`nav${i}`));
+}
+
+// Section content
+const sectionsContent = Array.from(
+  // Put your sections ID's here
+  document.querySelectorAll("#page1, #page2, #page3, #page4, #page5")
+);
+
+// Define a function to handle section link clicks
+function handleSectionLinkClick(index) {
+  // Set default text decoration for all links to none
+  sectionsLinks.forEach((link) => (link.style.textDecoration = "none"));
+  // Set text decoration for clicked link to underline
+  sectionsLinks[index].style.textDecoration = "underline";
+  // Hide all other sections except the displayed one
+  sectionsContent.forEach((section) => (section.style.display = "none"));
+  // Display the selected section
+  sectionsContent[index].style.display = "block";
+}
+
+// Set the default text decoration for the first link
+sectionsLinks[0].style.textDecoration = "underline";
+
+// Add event listeners to all section links
+sectionsLinks.forEach((link, index) => {
+  link.addEventListener("click", () => {
+    handleSectionLinkClick(index);
+  });
+});
